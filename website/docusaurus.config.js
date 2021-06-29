2e8c14e33d7b3fa5bf48a95256a4a1a418589c1f
Isaac <iicono27@gmail.com>
2021-06-28 23:51:27 -0500

    Update edit urls, make ostickstocalendartime file lowercase

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -94,15 +94,14 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: 'https://github.com/icogn/tp-docs/edit/main/website/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
