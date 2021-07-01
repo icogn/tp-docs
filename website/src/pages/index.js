c4fb7ff682cf5e21ce38328fb13afe7afa405d94
Isaac <iicono27@gmail.com>
2021-07-01 14:02:04 -0500

    Clean bgVideo code

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -41,6 +41,22 @@ function HomepageHeader() {
   );
 }
 
+function HomepageBackground() {
+  return (
+    <div className={styles.bgWrapper}>
+      <div
+        className={styles.bgImg}
+        style={{ backgroundImage: `url("${img}")` }}
+      ></div>
+      <video autoPlay muted loop className={styles.video}>
+        <source src={vid} type="video/webm" />
+        <source src={vidmp4} type="video/mp4" />
+      </video>
+      <div className={styles.bgOverlay}></div>
+    </div>
+  );
+}
+
 export default function Home() {
   useLayoutEffect(() => {
     // Prevent scrollbar from flashing if it isn't needed
@@ -70,25 +86,7 @@ export default function Home() {
       description="Twilight Princess at your fingertips"
       wrapperClassName={styles.root}
     >
-      <div
-        // className={clsx(styles.background, styles.bgImg)}
-        className={clsx(styles.videoWrapper, styles.bgImg)}
-        style={{ backgroundImage: `url("${img}")` }}
-      ></div>
-      <div className={styles.videoWrapper}>
-        <video
-          autoPlay
-          muted
-          loop
-          // className={clsx(styles.background, styles.video)}
-          className={styles.video}
-        >
-          <source src={vid} type="video/webm" />
-          <source src={vidmp4} type="video/mp4" />
-        </video>
-        <div className={styles.overlay2}></div>
-      </div>
-      {/* <div className={clsx(styles.background, styles.videoOverlay)}></div> */}
+      <HomepageBackground />
       <HomepageHeader />
       <main>
         <div
